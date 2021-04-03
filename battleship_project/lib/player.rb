9dd3fec04742209a3eb8d5fd2b1be2eb10cee58f
dani <danielstmontoya@gmail.com>
2021-04-03 12:47:07 -0500

    delete all

--- battleship_project/lib/player.rb
+++
@@ -1,7 +0,0 @@
-class Player
-  def get_move
-    puts 'enter a position with coordinates separated with a space like `4 7`'
-    position = gets.chomp
-    position.split.map {|el| el.to_i}
-  end
-end
